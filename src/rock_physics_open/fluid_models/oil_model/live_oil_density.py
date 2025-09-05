fix: enforce SI units in all fluid models, update snapshots
doc: compare results from fluid models with original illustrations in publication by jupyter notebook

src/rock_physics_open/fluid_models/oil_model/live_oil_density.py
@@ -14,17 +14,18 @@ def live_oil_density(
     Equation 24 in Batzle & Wang [1].
 
     :param reference_density: Density of the oil without dissolved gas
-        at 15.6 degrees Celsius and atmospheric pressure. (g/cc)
-    :param pressure: Pressure (MPa) of oil (for future implementation only)
+        at 15.6 degrees Celsius and atmospheric pressure. [kg/m^3]
+    :param pressure: Pressure [Pa] of oil (for future implementation only)
     :param gas_oil_ratio: The volume ratio of gas to oil [l/l]
-    :param temperature: Temperature (Celsius) of oil.
+    :param temperature: Temperature [°C] of oil.
     :param gas_gravity: molar mass of gas relative to air molar mas.
-    :return: Density of live oil [g/cc].
+    :return: Density of live oil [kg/m^3].
     """
+    density_gcc = reference_density / 1000.0
     b0 = live_oil_volume_factor(
         temperature, reference_density, gas_oil_ratio, gas_gravity
     )
-    return (reference_density + 0.0012 * gas_gravity * gas_oil_ratio) / b0
+    return 1000.0 * (density_gcc + 0.0012 * gas_gravity * gas_oil_ratio) / b0
 
 
 def live_oil_pseudo_density(
@@ -40,16 +41,17 @@ def live_oil_pseudo_density(
     Equation 22 in Batzle & Wang [1].
 
     :param reference_density: Density of the oil without dissolved gas
-        at 15.6 degrees Celsius and atmospheric pressure. (g/cc)
+        at 15.6 degrees Celsius and atmospheric pressure. [kg/m^3]
     :param gas_oil_ratio: The volume ratio of gas to oil [l/l]
-    :param temperature: Temperature (Celsius) of oil.
+    :param temperature: Temperature [°C] of oil.
     :param gas_gravity: molar mass of gas relative to air molar mas.
-    :return: Pseudo-density of live oil.
+    :return: Pseudo-density of live oil [kg/m^3].
     """
+    density_gcc = reference_density / 1000.0
     b0 = live_oil_volume_factor(
         temperature, reference_density, gas_oil_ratio, gas_gravity
     )
-    return (reference_density / b0) / (1 + 0.001 * gas_oil_ratio)
+    return 1000.0 * (density_gcc / b0) / (1 + 0.001 * gas_oil_ratio)
 
 
 def live_oil_volume_factor(
@@ -61,17 +63,18 @@ def live_oil_volume_factor(
     """
     Volume factor derived by Standing (1962), equation 23 in Batzle & Wang [1].
     :param reference_density: Density of the oil without dissolved gas
-        at 15.6 degrees Celsius and atmospheric pressure. (g/cc)
+        at 15.6 degrees Celsius and atmospheric pressure. [kg/m^3]
     :param gas_oil_ratio: The volume ratio of gas to oil [l/l]
-    :param temperature: Temperature (Celsius) of oil.
+    :param temperature: Temperature [°C] of oil.
     :param gas_gravity: molar mass of gas relative to air molar mas.
-    :return: A volume factor in calculating pseudo-density of live oil.
+    :return: A volume factor in calculating pseudo-density of live oil [unitless].
     """
+    density_gcc = reference_density / 1000.0
     return (
         0.972
         + 0.00038
         * (
-            2.4 * gas_oil_ratio * np.sqrt(gas_gravity / reference_density)
+            2.4 * gas_oil_ratio * np.sqrt(gas_gravity / density_gcc)
             + temperature
             + 17.8
         )