fix: enforce SI units in all fluid models, update snapshots
doc: compare results from fluid models with original illustrations in publication by jupyter notebook

src/rock_physics_open/fluid_models/brine_model/brine_properties.py
@@ -14,16 +14,16 @@ def brine_properties(
     p_cacl: np.ndarray | float | None = None,
 ) -> tuple[np.ndarray, np.ndarray, np.ndarray]:
     """
-    :param salinity: Salinity of solution as ppm of NaCl.
-    :param pressure: Pressure (Pa) of oil
-    :param temperature: Temperature (Celsius) of oil.
+    :param salinity: Salinity of solution as [ppm] of NaCl.
+    :param pressure: Pressure [Pa]
+    :param temperature: Temperature [°C]
     :param p_nacl: NaCl percentage, for future use
     :param p_kcl: KCl percentage, for future use
     :param p_cacl: CaCl percentage, for future use
-    :return: vel_b [m/s], den_b [kg/m^3], k_b [Pa]
+    :return: Brine velocity vel_b [m/s], brine density den_b [kg/m^3], brine bulk modulus k_b [Pa]
     """
-    vel_b = brine_primary_velocity(temperature, pressure * 1e-6, salinity * 1e-6)
-    den_b = brine_density(temperature, pressure * 1e-6, salinity * 1e-6) * 1000
+    vel_b = brine_primary_velocity(temperature, pressure, salinity)
+    den_b = brine_density(temperature, pressure, salinity)
     k_b = vel_b**2 * den_b
     return vel_b, den_b, k_b
 
@@ -35,19 +35,27 @@ def brine_density(
 ) -> np.ndarray | float:
     """
     density of sodium chloride solutions, equation 27 in Batzle & Wang [1].
-    :param salinity: Salinity of solution as weight fraction (ppm/1000000) of
-        sodium chloride.
-    :param pressure: Pressure (MPa) of oil
-    :param temperature: Temperature (Celsius) of oil.
-    :return: density of solution in g/cc.
+    :param salinity: Salinity of solution in ppm
+    :param pressure: Pressure [Pa]
+    :param temperature: Temperature [°C]
+    :return: density of solution in [kg/m^3].
     """
+    # Change unit of pressure to MPa
+    pressure_mpa = pressure / 1.0e6
+    # Change unit of salinity to fraction
+    salinity_frac = salinity / 1.0e6
+
     coefficients = [
         [[0.668, 3e-4], [8e-5, -13e-6], [3e-6, 0.0]],
         [[0.44, -24e-4], [-33e-4, 47e-6], [0.0, 0.0]],
     ]
-    return water_density(temperature, pressure) + salinity * polyval3d(
-        salinity, temperature, pressure, coefficients
+    water_den = water_density(temperature, pressure)
+    brine_correction = (
+        salinity_frac
+        * polyval3d(salinity_frac, temperature, pressure_mpa, coefficients)
+        * 1000.0
     )
+    return water_den + brine_correction
 
 
 def brine_primary_velocity(
@@ -58,12 +66,16 @@ def brine_primary_velocity(
     """
     Primary wave velocity of sodium chloride solutions, equation 29 in Batzle & Wang [1]
 
-    :param salinity: Salinity of solution as weight fraction (ppm/1000000) of
-        sodium chloride.
-    :param pressure: Pressure (MPa) of oil
-    :param temperature: Temperature (Celsius) of oil.
+    :param salinity: Salinity of solution as [ppm] of sodium chloride
+    :param pressure: Pressure [Pa]
+    :param temperature: Temperature (Celsius)
     :return: velocity of solution in m/s.
     """
+    # Change unit for salinity from ppm to fraction
+    salinity_frac = salinity / 1.0e6
+    # Change the unit for pressure from Pa to MPa
+    pressure_mpa = pressure / 1.0e6
+
     coefficients = np.zeros((3, 4, 3))
     coefficients[0, 0, 0] = 1170
     coefficients[0, 1, 0] = -9.6
@@ -77,8 +89,8 @@ def brine_primary_velocity(
     coefficients[1, 0, 2] = 0.16
     coefficients[2, 0, 0] = -820
 
-    return water_primary_velocity(temperature, pressure) + salinity * polyval3d(
-        sqrt(salinity), temperature, pressure, coefficients
+    return water_primary_velocity(temperature, pressure) + salinity_frac * polyval3d(
+        sqrt(salinity_frac), temperature, pressure_mpa, coefficients
     )
 
 
@@ -88,17 +100,20 @@ def water_density(
 ) -> np.ndarray | float:
     """
     Density of water,, equation 27a in Batzle & Wang [1].
-    :param pressure: Pressure (MPa) of oil
-    :param temperature: Temperature (Celsius) of oil.
-    :return: Density of water in g/cc.
+    :param pressure: Pressure [Pa]
+    :param temperature: Temperature [°C]
+    :return: Density of water in [kg/m^3].
     """
+    # Change unit of pressure from Pa to MPa
+    pressure_mpa = pressure / 1.0e6
+
     coefficients = [
         [1.0, 489e-6, -333e-9],
         [-8e-5, -2e-6, -2e-09],
         [-33e-7, 16e-9, 0.0],
         [1.75e-9, -13e-12, 0.0],
     ]
-    return polyval2d(temperature, pressure, coefficients)
+    return polyval2d(temperature, pressure_mpa, coefficients) * 1000.0
 
 
 def water_primary_velocity(
@@ -107,11 +122,14 @@ def water_primary_velocity(
 ) -> np.ndarray | float:
     """
     Primary wave velocity of water, table 1 and equation 28 in Batzle & Wang [1].
-    :param pressure: Pressure (MPa) of oil
-    :param temperature: Temperature (Celsius) of oil.
+    :param pressure: Pressure [Pa]
+    :param temperature: Temperature [°C]
     :return: primary wave velocity of water in m/s.
     """
-    if np.any(pressure > 100):
+    # Change unit of pressure from Pa to MPa
+    pressure_mpa = pressure / 1.0e6
+
+    if np.any(pressure_mpa > 100):
         warnings.warn(
             "Calculations for water velocity is not precise for\n"
             + "pressure outside [0,100]MPa"
@@ -125,19 +143,36 @@ def water_primary_velocity(
         [1.487e-4, -6.503e-7, -1.455e-8, 1.327e-10],
         [-2.197e-7, 7.987e-10, 5.23e-11, -4.614e-13],
     ]
-    return polyval2d(temperature, pressure, coefficients)
+    return polyval2d(temperature, pressure_mpa, coefficients)
 
 
 def water(
     temperature: np.ndarray | float, pressure: np.ndarray | float
 ) -> tuple[np.ndarray, np.ndarray, np.ndarray]:
     """
-    :param pressure: Pressure (Pa) of oil
-    :param temperature: Temperature (Celsius) of oil.
-    :return: water_density [kg/m^3], water_velocity [m/s], water_bulk_modulus [Pa]
+    :param pressure: Pressure [Pa]
+    :param temperature: Temperature [°C]
+    :return: water_velocity [m/s], water_density [kg/m^3], water_bulk_modulus [Pa]
     """
-    pressure_mpa = pressure * 1.0e-6
-    water_den = water_density(temperature, pressure_mpa)
-    water_vel = water_primary_velocity(temperature, pressure_mpa)
-    water_k = water_vel**2 * water_den * 1000.0
-    return water_den, water_vel, water_k
+    water_den = water_density(temperature, pressure)
+    water_vel = water_primary_velocity(temperature, pressure)
+    water_k = water_vel**2 * water_den
+    return water_vel, water_den, water_k
+
+
+def brine_viscosity(
+    temperature: np.ndarray | float,
+    salinity: np.ndarray | float,
+) -> np.ndarray | float:
+    """
+    Brine viscosity according to Batzle & Wang [1].
+
+    Based on equation 32.
+    """
+    salinity_frac = salinity / 1.0e6
+    return (
+        0.1
+        + 0.333 * salinity_frac
+        + (1.65 + 91.9 * salinity_frac**3)
+        * np.exp(-(0.42 * (salinity_frac**0.8 - 0.17) ** 2 + 0.045) * temperature**0.8)
+    )

src/rock_physics_open/fluid_models/gas_model/gas_properties.py
@@ -4,7 +4,7 @@ from scipy.constants import gas_constant
 
 from rock_physics_open.equinor_utilities.conversions import celsius_to_kelvin
 
-AIR_WEIGHT = 28.8  # g/mol
+AIR_WEIGHT = 28.8 * 1.0e-3  # kg/mol
 
 
 def gas_properties(
@@ -20,10 +20,11 @@ def gas_properties(
     :param model: for future use
     :return: vel_gas [m/s], den_gas [kg/m^3], k_gas [Pa], eta_gas [cP]
     """
-    den_gas = gas_density(celsius_to_kelvin(temperature), pressure * 1e-6, gas_gravity)
-    k_gas = gas_bulk_modulus(
-        celsius_to_kelvin(temperature), pressure * 1e-6, gas_gravity
-    )
+
+    den_gas = gas_density(celsius_to_kelvin(temperature), pressure, gas_gravity)
+
+    k_gas = gas_bulk_modulus(celsius_to_kelvin(temperature), pressure, gas_gravity)
+
     vel_gas = (k_gas / den_gas) ** 0.5
 
     eta_gas = lee_gas_viscosity(celsius_to_kelvin(temperature), pressure, gas_gravity)
@@ -33,9 +34,9 @@ def gas_properties(
 
 def molecular_weight(gas_gravity: np.ndarray | float) -> np.ndarray | float:
     """
-    calculates molecluar weight of a gas from gas gravity.
+    calculates molecular weight of a gas from gas gravity.
     :param gas_gravity: molar mass of gas relative to air molar mas.
-    :return: The volume of the gas in g/mol.
+    :return: The volume of the gas in kg/mol.
     """
     return gas_gravity * AIR_WEIGHT
 
@@ -47,9 +48,10 @@ def molar_volume(
     """
     calculates molar volume using the ideal gas law.
     :param absolute_temperature: The absolute temperature of the gas in kelvin.
-    :param pressure: Confining pressure in MPa.
-    :return: The volume of the gas in cc/mol.
+    :param pressure: Confining pressure in Pa.
+    :return: The volume of the gas in m^3/mol.
     """
+
     return gas_constant * absolute_temperature / pressure
 
 
@@ -62,8 +64,8 @@ def ideal_gas_density(
     calculates molar volume using the ideal gas law.
     :param gas_gravity: molar mass of gas relative to air molar mas.
     :param absolute_temperature: The absolute temperature of the gas in kelvin.
-    :param pressure: Confining pressure in MPa.
-    :return: The density of the gas in g/cc
+    :param pressure: Confining pressure in Pa.
+    :return: The density of the gas in kg/m^3
     """
     return molecular_weight(gas_gravity) / molar_volume(absolute_temperature, pressure)
 
@@ -89,13 +91,11 @@ def ideal_gas(
     :param gas_gravity: molar mass of gas relative to air molar mas.
     :param absolute_temperature: The absolute temperature of the gas in kelvin.
     :param pressure: Confining pressure in Pa.
-    :return: ideal_gas_density, ideal_gas_velocity
+    :return: ideal_gas_velocity [m/s], ideal_gas_density [kg/m^3],
     """
-    ideal_gas_den = 1000 * ideal_gas_density(
-        absolute_temperature, pressure * 1e6, gas_gravity
-    )
+    ideal_gas_den = ideal_gas_density(absolute_temperature, pressure, gas_gravity)
     ideal_gas_vel = ideal_gas_primary_velocity(absolute_temperature, gas_gravity)
-    return ideal_gas_den, ideal_gas_vel
+    return ideal_gas_vel, ideal_gas_den
 
 
 def pseudoreduced_temperature(
@@ -132,13 +132,13 @@ def pseudoreduced_pressure(
     Tech., 22, 889-892.
 
     :param gas_gravity: molar mass of gas relative to air molar mas.
-    :param pressure: Confining pressure in MPa.
-    :return: Pseudoreduced pressure in MPa.
+    :param pressure: Confining pressure in Pa.
+    :return: Pseudoreduced pressure in Pa.
     """
     return pressure / (4.892 - 0.4048 * gas_gravity)
 
 
-def compressability_factor(
+def compressibility_factor(
     absolute_temperature: np.ndarray | float,
     pressure: np.ndarray | float,
     gas_gravity: np.ndarray | float,
@@ -149,11 +149,13 @@ def compressability_factor(
 
     :param gas_gravity: molar mass of gas relative to air molar mas.
     :param absolute_temperature: The absolute temperature of the gas in kelvin.
-    :param pressure: Confining pressure in MPa.
-    :return: The density of the gas in g/cc
+    :param pressure: Confining pressure in Pa.
+    :return: Gas compressibility - unitless
     """
     tpr = pseudoreduced_temperature(absolute_temperature, gas_gravity)
-    ppr = pseudoreduced_pressure(pressure, gas_gravity)
+
+    # Pseudoreduced pressure has unit MPa in equation
+    ppr = pseudoreduced_pressure(pressure, gas_gravity) * 1.0e-6
 
     return (
         (0.03 + 0.00527 * (3.5 - tpr) ** 3) * ppr
@@ -162,7 +164,7 @@ def compressability_factor(
         - 0.52
         + 0.109
         * (3.85 - tpr) ** 2
-        / exp((0.45 + 8 * (0.56 - 1 / tpr) ** 2) * ppr**1.2 / tpr)
+        / exp((0.45 + 8.0 * (0.56 - 1 / tpr) ** 2) * ppr**1.2 / tpr)
     )
 
 
@@ -176,18 +178,17 @@ def gas_density(
 
     :param gas_gravity: molar mass of gas relative to air molar mas.
     :param absolute_temperature: The absolute temperature of the gas in kelvin.
-    :param pressure: Confining pressure in MPa.
-    :return: The density of the gas in g/cc
+    :param pressure: Confining pressure in Pa.
+    :return: The density of the gas in kg/m^3
     """
-    ideal_gas_den, ideal_gas_vel = ideal_gas(
-        absolute_temperature, pressure * 1e-6, gas_gravity
-    )
-    return ideal_gas_den / compressability_factor(
+
+    _, ideal_gas_den = ideal_gas(absolute_temperature, pressure, gas_gravity)
+    return ideal_gas_den / compressibility_factor(
         absolute_temperature, pressure, gas_gravity
     )
 
 
-def compressability_rate_per_pseudoreduced_pressure(
+def compressibility_rate_per_pseudoreduced_pressure(
     absolute_temperature: np.ndarray | float,
     pressure: np.ndarray | float,
     gas_gravity: np.ndarray | float,
@@ -201,7 +202,9 @@ def compressability_rate_per_pseudoreduced_pressure(
     :return: The density of the gas in g/cc
     """
     tpr = pseudoreduced_temperature(absolute_temperature, gas_gravity)
-    ppr = pseudoreduced_pressure(pressure, gas_gravity)
+
+    # Pseudoreduced pressure is expected to be in MPa in the expression
+    ppr = pseudoreduced_pressure(pressure, gas_gravity) * 1.0e-6
 
     return (
         0.03
@@ -226,15 +229,16 @@ def gas_bulk_modulus(
 
     :param gas_gravity: molar mass of gas relative to air molar mas.
     :param absolute_temperature: The absolute temperature of the gas in kelvin.
-    :param pressure: Confining pressure in MPa.
-    :return: The bulk modulus of the gas in MPa.
+    :param pressure: Confining pressure in Pa.
+    :return: The bulk modulus of the gas in Pa.
     """
-    z = compressability_factor(absolute_temperature, pressure, gas_gravity)
-    dz_dppr = compressability_rate_per_pseudoreduced_pressure(
+    z = compressibility_factor(absolute_temperature, pressure, gas_gravity)
+    dz_dppr = compressibility_rate_per_pseudoreduced_pressure(
         absolute_temperature, pressure, gas_gravity
     )
 
-    ppr = pseudoreduced_pressure(pressure, gas_gravity)
+    # Set ppr in unit MPa in order to use it in calculation of gamma_0
+    ppr = pseudoreduced_pressure(pressure, gas_gravity) * 1.0e-6
 
     # Equation 11b
     gamma_0 = (
@@ -245,6 +249,44 @@ def gas_bulk_modulus(
     )
 
     return gamma_0 * pressure / (1 - dz_dppr * ppr / z)
+
+
+def gas_viscosity(
+    absolute_temperature: np.ndarray | float,
+    pressure: np.ndarray | float,
+    gas_gravity: np.ndarray | float,
+) -> np.ndarray | float:
+    """
+    The gas viscosity of hydrocarbon gas, using equations 12 and 13 of Batzle & Wang [1].
+
+    :param absolute_temperature: The absolute temperature of the gas in kelvin.
+    :param pressure: Confining pressure in Pa.
+    :param gas_gravity: molar mass of gas relative to air mas.
+    :return: The gas viscosity of the gas in cP.
+    """
+    temp_pr = pseudoreduced_temperature(absolute_temperature, gas_gravity)
+
+    # Pseudoreduced pressure should be in unit MPa
+    pres_pr = pseudoreduced_pressure(pressure, gas_gravity) * 1.0e-6
+
+    eta_1 = 0.0001 * (
+        temp_pr * (28.0 + 48.0 * gas_gravity - 5.0 * gas_gravity**2)
+        - 6.47 * gas_gravity**-2
+        + 35.0 * gas_gravity**-1
+        + 1.14 * gas_gravity
+        - 15.55
+    )
+    return eta_1 * (
+        0.001
+        * pres_pr
+        * (
+            (1057.0 - 8.08 * temp_pr) / pres_pr
+            + (796.0 * pres_pr**0.5 - 704.0)
+            / (((temp_pr - 1.0) ** 0.7) * (pres_pr + 1.0))
+            - 3.24 * temp_pr
+            - 38.0
+        )
+    )
 
 
 def lee_gas_viscosity(

src/rock_physics_open/fluid_models/oil_model/dead_oil_velocity.py
@@ -12,17 +12,19 @@ def dead_oil_velocity(
     Uses equation 20a from Batzle & Wang [1].
 
     :param reference_density: Density of oil at 15.6 degrees Celsius and atmospheric
-        pressure (g/cc)
-    :param pressure: Pressure (MPa) of oil
-    :param temperature: Temperature (Celsius) of oil.
+        pressure [kg/m^3]
+    :param pressure: Pressure [Pa] of oil
+    :param temperature: Temperature [°C] of oil.
     :return: primary velocity of dead oil in m/s.
     """
+    pressure_mpa = pressure * 1e-6
+    density_gcc = reference_density / 1000.0
     return (
-        2096 * np.sqrt(reference_density / (2.6 - reference_density))
+        2096 * np.sqrt(density_gcc / (2.6 - density_gcc))
         - 3.7 * temperature
-        + 4.64 * pressure
+        + 4.64 * pressure_mpa
         + 0.0115
-        * (4.12 * np.sqrt(1.08 * reference_density**-1 - 1) - 1)
+        * (4.12 * np.sqrt(1.08 * density_gcc**-1 - 1) - 1)
         * temperature
-        * pressure
+        * pressure_mpa
     )

src/rock_physics_open/fluid_models/oil_model/oil_properties.py
@@ -17,13 +17,13 @@ def oil_properties(
     gas_gravity: np.ndarray | float,
 ) -> np.ndarray | float:
     """
-    :param temperature: Temperature (Celsius) of oil.
-    :param pressure: Pressure (Pa) of oil
+    :param temperature: Temperature [°C] of oil.
+    :param pressure: Pressure [Pa] of oil
     :param rho0: Density of the oil without dissolved gas at 15.6 degrees Celsius and
-                 atmospheric pressure. (kg/m^3)
+                 atmospheric pressure. [kg/m^3]
     :param gas_oil_ratio: The volume ratio of gas to oil [l/l]
     :param gas_gravity: Gas Gravity, molar mass of gas relative to air molar mas.
-    :return: vel_oil, den_oil, k_oil
+    :return: vel_oil [m/s], den_oil [kg/m^3], k_oil [Pa]
     """
     # Since live_oil with gas_oil_ratio=0.0 is not equal to dead oil
     # we use an apodization function to interpolate between the two
@@ -41,10 +41,11 @@ def oil_properties(
         window = np.clip((np.abs(x) - length / 2) / (length / 2), 0, 1)
         return 1 - window
 
-    loil_den, loil_vel = live_oil(
-        temperature, pressure, rho0, gas_oil_ratio, gas_gravity
-    )
-    doil_den, doil_vel = dead_oil(temperature, pressure, rho0)
+    (
+        loil_vel,
+        loil_den,
+    ) = live_oil(temperature, pressure, rho0, gas_oil_ratio, gas_gravity)
+    doil_vel, doil_den = dead_oil(temperature, pressure, rho0)
     window = triangular_window(gas_oil_ratio)
     den_oil = doil_den * window + (1 - window) * loil_den
     vel_oil = doil_vel * window + (1 - window) * loil_vel
@@ -59,20 +60,16 @@ def dead_oil(
 ) -> tuple[np.ndarray | float, np.ndarray | float]:
     """
     :param reference_density: Density of the oil without dissolved gas
-        at 15.6 degrees Celsius and atmospheric pressure. kg/m3
+        at 15.6 degrees Celsius and atmospheric pressure. [kg/m^3]
     :param gas_oil_ratio: The volume ratio of gas to oil [l/l]
     :param gas_gravity: molar mass of gas relative to air molar mas.
-    :param pressure: Pressure (Pa) of oil
-    :param temperature: Temperature (Celsius) of oil.
-    :return: dead_oil_density, dead_oil_velocity
+    :param pressure: Pressure [Pa] of oil
+    :param temperature: Temperature [°C] of oil.
+    :return: dead_oil_density [kg/m^3], dead_oil_velocity [m/s]
     """
-    dead_oil_den = 1000 * dead_oil_density(
-        temperature, pressure * 1e-6, reference_density / 1000
-    )
-    dead_oil_vel = dead_oil_velocity(
-        temperature, pressure * 1e-6, reference_density / 1000
-    )
-    return dead_oil_den, dead_oil_vel
+    dead_oil_den = dead_oil_density(temperature, pressure, reference_density)
+    dead_oil_vel = dead_oil_velocity(temperature, pressure, reference_density)
+    return dead_oil_vel, dead_oil_den
 
 
 def live_oil(
@@ -84,12 +81,12 @@ def live_oil(
 ) -> tuple[np.ndarray | float, np.ndarray | float]:
     """
     :param reference_density: Density of the oil without dissolved gas
-        at 15.6 degrees Celsius and atmospheric pressure. (kg/m^3)
+        at 15.6 degrees Celsius and atmospheric pressure. [kg/m^3]
     :param gas_oil_ratio: The volume ratio of gas to oil [l/l]
     :param gas_gravity: molar mass of gas relative to air molar mas.
-    :param pressure: Pressure (Pa) of oil
-    :param temperature: Temperature (Celsius) of oil.
-    :return: live_oil_density, live_oil_velocity
+    :param pressure: Pressure [Pa] of oil
+    :param temperature: Temperature [°C] of oil.
+    :return: live_oil_density , live_oil_velocity
     """
     if np.any(
         pressure
@@ -99,18 +96,51 @@ def live_oil(
             "Pressure is below bubble point of oil, estimated elastic properties can be inaccurate",
             stacklevel=1,
         )
-    live_oil_den = 1000 * live_oil_density(
+    live_oil_den = live_oil_density(
         temperature,
-        pressure * 1e-6,
-        reference_density / 1000,
+        pressure,
+        reference_density,
         gas_oil_ratio,
         gas_gravity,
     )
     live_oil_vel = live_oil_velocity(
         temperature,
-        pressure * 1e-6,
-        reference_density / 1000,
+        pressure,
+        reference_density,
         gas_oil_ratio,
         gas_gravity,
     )
-    return live_oil_den, live_oil_vel
+    return (
+        live_oil_vel,
+        live_oil_den,
+    )
+
+
+def oil_viscosity(
+    temperature: np.ndarray | float,
+    pressure: np.ndarray | float,
+    reference_density: np.ndarray | float,
+) -> np.ndarray | float:
+    """
+    Calculate dead oil viscosity. If dissolved gas is present in the oil, the reference density
+    should be substituted by live oil density.
+
+    Equations 25a, 25b, 26a & 26b in Batzle and Wang 1992
+
+    Based on Beggs and Robinson 1975
+
+    :param temperature: Temperature [°C] of oil
+    :param pressure: Pressure [Pa] of oil
+    :param reference_density: Density of the oil without dissolved gas
+    """
+    # Change unit in pressure to MPa
+    pressure_mpa = pressure / 1.0e6
+    # Change unit in density to g/cc
+    density_gcc = reference_density / 1000.0
+
+    y_factor = 10 ** (5.693 - 2.863 / density_gcc)
+    eta_t = -1.0 + 10 ** (0.505 * y_factor * (17.8 + temperature) ** -1.163)
+    i_factor = 10 ** (
+        18.6 * (0.1 * np.log10(eta_t) + (np.log10(eta_t) + 2) ** -0.1 - 0.985)
+    )
+    return eta_t + 0.145 * pressure_mpa * i_factor

tests/fluid_model_tests/test_brine_properties.py
@@ -21,7 +21,6 @@ from rock_physics_open.fluid_models.brine_model.brine_properties import (
 )
 
 pres = 23.0e6 * np.linspace(0.8, 1.2, 101)
-pres_mpa = pres * 1.0e-6
 temp = 100.0 * np.linspace(0.8, 1.2, 101)
 sal = 35000.0 * np.ones(101)
 nacl = 80.0 * np.ones(101)
@@ -49,7 +48,7 @@ def test_water_properties():
 
 
 def test_water_density():
-    args = water_density(temp, pres_mpa)
+    args = water_density(temp, pres)
 
     if not os.path.isfile(get_snapshot_name()) or INITIATE:
         store_snapshot(get_snapshot_name(), args)
@@ -58,7 +57,7 @@ def test_water_density():
 
 
 def test_water_velocity():
-    args = water_primary_velocity(temp, pres_mpa)
+    args = water_primary_velocity(temp, pres)
 
     if not os.path.isfile(get_snapshot_name()) or INITIATE:
         store_snapshot(get_snapshot_name(), args)
@@ -67,7 +66,7 @@ def test_water_velocity():
 
 
 def test_brine_density():
-    args = brine_density(temp, pres_mpa, sal)
+    args = brine_density(temp, pres, sal)
 
     if not os.path.isfile(get_snapshot_name()) or INITIATE:
         store_snapshot(get_snapshot_name(), args)
@@ -76,7 +75,7 @@ def test_brine_density():
 
 
 def test_brine_velocity():
-    args = brine_primary_velocity(temp, pres_mpa, sal)
+    args = brine_primary_velocity(temp, pres, sal)
 
     if not os.path.isfile(get_snapshot_name()) or INITIATE:
         store_snapshot(get_snapshot_name(), args)
@@ -90,11 +89,11 @@ def test_water_brine_properties_float():
     """
     for results in [
         water(temp[0], pres[0]),
-        water_density(temp[0], pres_mpa[0]),
-        water_primary_velocity(temp[0], pres_mpa[0]),
+        water_density(temp[0], pres[0]),
+        water_primary_velocity(temp[0], pres[0]),
         brine_properties(temp[0], pres[0], sal[0]),
-        brine_density(temp[0], pres_mpa[0], sal[0]),
-        brine_primary_velocity(temp[0], pres_mpa[0], sal[0]),
+        brine_density(temp[0], pres[0], sal[0]),
+        brine_primary_velocity(temp[0], pres[0], sal[0]),
     ]:
         if hasattr(results, "__iter__"):
             assert all(isinstance(arg, float) for arg in results)

tests/fluid_model_tests/test_oil_properties.py
@@ -20,8 +20,6 @@ gor = 120.0 * np.ones(101)
 gr = 0.7 * np.ones(101)
 gor_co2 = 50.0 * np.ones(101)
 gr_co2 = 1.52 * np.ones(101)
-pres_mpa = 30.0 * np.linspace(0.8, 1.2, 101)
-rho_0_gcc = 0.850 * np.ones(101)
 
 
 def test_oil_prop():
@@ -34,7 +32,7 @@ def test_oil_prop():
 
 
 def test_live_oil_density():
-    args = live_oil_density(temp, pres_mpa, rho_0_gcc, gor, gr)
+    args = live_oil_density(temp, pres, rho_0, gor, gr)
 
     if not os.path.isfile(get_snapshot_name()) or INITIATE:
         store_snapshot(get_snapshot_name(), args)
@@ -43,7 +41,7 @@ def test_live_oil_density():
 
 
 def test_live_oil_velocity():
-    args = live_oil_velocity(temp, pres_mpa, rho_0_gcc, gor, gr)
+    args = live_oil_velocity(temp, pres, rho_0, gor, gr)
 
     if not os.path.isfile(get_snapshot_name()) or INITIATE:
         store_snapshot(get_snapshot_name(), args)
@@ -57,8 +55,8 @@ def test_oil_properties_float():
     """
     for results in [
         oil_properties(temp[0], pres[0], rho_0[0], gor[0], gr[0]),
-        live_oil_density(temp[0], pres_mpa[0], rho_0_gcc[0], gor[0], gr[0]),
-        live_oil_velocity(temp[0], pres_mpa[0], rho_0_gcc[0], gor[0], gr[0]),
+        live_oil_density(temp[0], pres[0], rho_0[0], gor[0], gr[0]),
+        live_oil_velocity(temp[0], pres[0], rho_0[0], gor[0], gr[0]),
     ]:
         if hasattr(results, "__iter__"):
             assert all(isinstance(arg, float) for arg in results)