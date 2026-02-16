4bd5493068907da3a1aeef536596447c920d858e
Einar W. Arbo <earb@equinor.com>
2026-02-16 09:52:12 +0000

    refactor: remove sympy dependency
    Pre-compute the 24 Span & Wagner Helmholtz energy expressions and their
    derivatives (up to 2nd order in tau and delta) as pure numpy functions,
    replacing the runtime sympy symbolic differentiation and lambdify calls.
    
    This removes ~49 MB of installed dependencies (sympy + mpmath) and
    eliminates the ~2.8s import-time cost of symbolic computation.
    
    Closes #44

--- src/rock_physics_open/span_wagner/equations.py
+++ src/rock_physics_open/span_wagner/equations.py
@@ -1,15 +1,439 @@
 """
-Package for calculating the residual helmholtz energy for CO2. The module uses sympy to evaluate the functions defined
-in Span & Wagner [2], as well as its derivatives.
+Package for calculating the residual helmholtz energy for CO2.
+
+Pre-computed numpy implementations of the Span & Wagner [2] equations and their
+derivatives (up to 2nd order with respect to tau and delta). These were originally
+generated via sympy symbolic differentiation and lambdify, and are now hardcoded
+as pure numpy expressions to avoid the sympy runtime dependency.
 """
 
-from typing import Any, Callable, Literal
+from collections.abc import Callable
+from typing import Any
 
 import numpy as np
 import numpy.typing as npt
-import sympy as sp
 
-from .coefficients import coeff_vars
+from .coefficients import (
+    A4,
+    B4,
+    C4,
+    D4,
+    a4,
+    alpha3,
+    b4,
+    beta3,
+    beta4,
+    c2,
+    d1,
+    d2,
+    d3,
+    epsilon3,
+    gamma3,
+    n1,
+    n2,
+    n3,
+    n4,
+    t1,
+    t2,
+    t3,
+)
+
+
+def _dirac_delta(x: npt.NDArray[np.float64]) -> npt.NDArray[np.float64]:
+    """Discrete Dirac delta: returns 1 where x == 0, else 0."""
+    return (x == 0).astype(np.float64)
+
+
+# ---------------------------------------------------------------------------
+# S1: n1 * delta^d1 * tau^t1
+# ---------------------------------------------------------------------------
+
+
+def _s1_dd0_dt0(
+    tau: npt.NDArray[np.float64], delta: npt.NDArray[np.float64]
+) -> npt.NDArray[np.float64]:
+    return delta**d1 * n1 * tau**t1
+
+
+def _s1_dd0_dt1(
+    tau: npt.NDArray[np.float64], delta: npt.NDArray[np.float64]
+) -> npt.NDArray[np.float64]:
+    return delta**d1 * n1 * t1 * tau ** (t1 - 1)
+
+
+def _s1_dd0_dt2(
+    tau: npt.NDArray[np.float64], delta: npt.NDArray[np.float64]
+) -> npt.NDArray[np.float64]:
+    return delta**d1 * n1 * t1 * tau ** (t1 - 2) * (t1 - 1)
+
+
+def _s1_dd1_dt0(
+    tau: npt.NDArray[np.float64], delta: npt.NDArray[np.float64]
+) -> npt.NDArray[np.float64]:
+    return d1 * delta ** (d1 - 1) * n1 * tau**t1
+
+
+def _s1_dd1_dt1(
+    tau: npt.NDArray[np.float64], delta: npt.NDArray[np.float64]
+) -> npt.NDArray[np.float64]:
+    return d1 * delta ** (d1 - 1) * n1 * t1 * tau ** (t1 - 1)
+
+
+def _s1_dd2_dt0(
+    tau: npt.NDArray[np.float64], delta: npt.NDArray[np.float64]
+) -> npt.NDArray[np.float64]:
+    return d1 * delta ** (d1 - 2) * n1 * tau**t1 * (d1 - 1)
+
+
+# ---------------------------------------------------------------------------
+# S2: n2 * delta^d2 * tau^t2 * exp(-delta^c2)
+# ---------------------------------------------------------------------------
+
+
+def _s2_dd0_dt0(
+    tau: npt.NDArray[np.float64], delta: npt.NDArray[np.float64]
+) -> npt.NDArray[np.float64]:
+    return delta**d2 * n2 * tau**t2 * np.exp(-(delta**c2))
+
+
+def _s2_dd0_dt1(
+    tau: npt.NDArray[np.float64], delta: npt.NDArray[np.float64]
+) -> npt.NDArray[np.float64]:
+    return delta**d2 * n2 * t2 * tau ** (t2 - 1) * np.exp(-(delta**c2))
+
+
+def _s2_dd0_dt2(
+    tau: npt.NDArray[np.float64], delta: npt.NDArray[np.float64]
+) -> npt.NDArray[np.float64]:
+    return delta**d2 * n2 * t2 * tau ** (t2 - 2) * (t2 - 1) * np.exp(-(delta**c2))
+
+
+def _s2_dd1_dt0(
+    tau: npt.NDArray[np.float64], delta: npt.NDArray[np.float64]
+) -> npt.NDArray[np.float64]:
+    return -c2 * delta ** (c2 + d2 - 1) * n2 * tau**t2 * np.exp(
+        -(delta**c2)
+    ) + d2 * delta ** (d2 - 1) * n2 * tau**t2 * np.exp(-(delta**c2))
+
+
+def _s2_dd1_dt1(
+    tau: npt.NDArray[np.float64], delta: npt.NDArray[np.float64]
+) -> npt.NDArray[np.float64]:
+    return (
+        delta ** (d2 - 1)
+        * n2
+        * t2
+        * tau ** (t2 - 1)
+        * (-c2 * delta**c2 + d2)
+        * np.exp(-(delta**c2))
+    )
+
+
+def _s2_dd2_dt0(
+    tau: npt.NDArray[np.float64], delta: npt.NDArray[np.float64]
+) -> npt.NDArray[np.float64]:
+    return (
+        delta ** (d2 - 2)
+        * n2
+        * tau**t2
+        * (
+            -2 * c2 * d2 * delta**c2
+            + c2 * delta**c2 * (c2 * delta**c2 - c2 + 1)
+            + d2 * (d2 - 1)
+        )
+        * np.exp(-(delta**c2))
+    )
+
+
+# ---------------------------------------------------------------------------
+# S3: n3 * delta^d3 * tau^t3 * exp(-alpha3*(delta-eps3)^2 - beta3*(tau-gamma3)^2)
+# ---------------------------------------------------------------------------
+
+
+def _s3_exp(
+    delta: npt.NDArray[np.float64], tau: npt.NDArray[np.float64]
+) -> npt.NDArray[np.float64]:
+    """Common exponential factor for s3 terms."""
+    return np.exp(-alpha3 * (delta - epsilon3) ** 2 - beta3 * (-gamma3 + tau) ** 2)
+
+
+def _s3_dd0_dt0(
+    tau: npt.NDArray[np.float64], delta: npt.NDArray[np.float64]
+) -> npt.NDArray[np.float64]:
+    return delta**d3 * n3 * tau**t3 * _s3_exp(delta, tau)
+
+
+def _s3_dd0_dt1(
+    tau: npt.NDArray[np.float64], delta: npt.NDArray[np.float64]
+) -> npt.NDArray[np.float64]:
+    e = _s3_exp(delta, tau)
+    return (
+        -beta3 * delta**d3 * n3 * tau**t3 * (-2 * gamma3 + 2 * tau) * e
+        + delta**d3 * n3 * t3 * tau ** (t3 - 1) * e
+    )
+
+
+def _s3_dd0_dt2(
+    tau: npt.NDArray[np.float64], delta: npt.NDArray[np.float64]
+) -> npt.NDArray[np.float64]:
+    return (
+        delta**d3
+        * n3
+        * tau**t3
+        * (
+            4 * beta3 * t3 * (gamma3 - tau) / tau
+            + 2 * beta3 * (2 * beta3 * (gamma3 - tau) ** 2 - 1)
+            + t3 * (t3 - 1) / tau**2
+        )
+        * _s3_exp(delta, tau)
+    )
+
+
+def _s3_dd1_dt0(
+    tau: npt.NDArray[np.float64], delta: npt.NDArray[np.float64]
+) -> npt.NDArray[np.float64]:
+    e = _s3_exp(delta, tau)
+    return (
+        -alpha3 * delta**d3 * n3 * tau**t3 * (2 * delta - 2 * epsilon3) * e
+        + d3 * delta ** (d3 - 1) * n3 * tau**t3 * e
+    )
+
+
+def _s3_dd1_dt1(
+    tau: npt.NDArray[np.float64], delta: npt.NDArray[np.float64]
+) -> npt.NDArray[np.float64]:
+    return (
+        delta**d3
+        * n3
+        * tau**t3
+        * (
+            -4 * alpha3 * beta3 * (delta - epsilon3) * (gamma3 - tau)
+            - 2 * alpha3 * t3 * (delta - epsilon3) / tau
+            + 2 * beta3 * d3 * (gamma3 - tau) / delta
+            + d3 * t3 / (delta * tau)
+        )
+        * _s3_exp(delta, tau)
+    )
+
+
+def _s3_dd2_dt0(
+    tau: npt.NDArray[np.float64], delta: npt.NDArray[np.float64]
+) -> npt.NDArray[np.float64]:
+    return (
+        delta**d3
+        * n3
+        * tau**t3
+        * (
+            -4 * alpha3 * d3 * (delta - epsilon3) / delta
+            + 2 * alpha3 * (2 * alpha3 * (delta - epsilon3) ** 2 - 1)
+            + d3 * (d3 - 1) / delta**2
+        )
+        * _s3_exp(delta, tau)
+    )
+
+
+# ---------------------------------------------------------------------------
+# S4: n4 * bigdelta^b4 * delta * exp(-C4*(delta-1)^2 - D4*(tau-1)^2)
+# where bigdelta = theta^2 + B4*|delta-1|^(2*a4)
+#       theta = (1 - tau) + A4*|delta-1|^(1/beta4)
+# ---------------------------------------------------------------------------
+
+
+def _s4_bigdelta(
+    tau: npt.NDArray[np.float64], delta: npt.NDArray[np.float64]
+) -> npt.NDArray[np.float64]:
+    """bigdelta = theta^2 + B4*|delta-1|^(2*a4)"""
+    return (
+        B4 * np.abs(delta - 1) ** (2 * a4)
+        + (A4 * np.abs(delta - 1) ** (beta4 ** (-1.0)) - tau + 1) ** 2
+    )
+
+
+def _s4_phi(
+    delta: npt.NDArray[np.float64], tau: npt.NDArray[np.float64]
+) -> npt.NDArray[np.float64]:
+    """phi = exp(-C4*(delta-1)^2 - D4*(tau-1)^2)"""
+    return np.exp(-C4 * (delta - 1) ** 2 - D4 * (tau - 1) ** 2)
+
+
+def _s4_dd0_dt0(
+    tau: npt.NDArray[np.float64], delta: npt.NDArray[np.float64]
+) -> npt.NDArray[np.float64]:
+    return delta * n4 * _s4_bigdelta(tau, delta) ** b4 * _s4_phi(delta, tau)
+
+
+def _s4_dd0_dt1(
+    tau: npt.NDArray[np.float64], delta: npt.NDArray[np.float64]
+) -> npt.NDArray[np.float64]:
+    bd = _s4_bigdelta(tau, delta)
+    phi = _s4_phi(delta, tau)
+    return (
+        -D4 * delta * n4 * (2 * tau - 2) * bd**b4 * phi
+        + b4
+        * delta
+        * n4
+        * bd ** (b4 - 1)
+        * (-2 * A4 * np.abs(delta - 1) ** (beta4 ** (-1.0)) + 2 * tau - 2)
+        * phi
+    )
+
+
+def _s4_dd0_dt2(
+    tau: npt.NDArray[np.float64], delta: npt.NDArray[np.float64]
+) -> npt.NDArray[np.float64]:
+    bd = _s4_bigdelta(tau, delta)
+    phi = _s4_phi(delta, tau)
+    theta_val = A4 * np.abs(delta - 1) ** (beta4 ** (-1.0)) - tau + 1
+    return (
+        2
+        * delta
+        * n4
+        * bd**b4
+        * (
+            4 * D4 * b4 * (tau - 1) * theta_val / bd
+            + D4 * (2 * D4 * (tau - 1) ** 2 - 1)
+            + b4 * (2 * b4 * theta_val**2 / bd + 1 - 2 * theta_val**2 / bd) / bd
+        )
+        * phi
+    )
+
+
+def _s4_dd1_dt0(
+    tau: npt.NDArray[np.float64], delta: npt.NDArray[np.float64]
+) -> npt.NDArray[np.float64]:
+    bd = _s4_bigdelta(tau, delta)
+    phi = _s4_phi(delta, tau)
+    theta_val = A4 * np.abs(delta - 1) ** (beta4 ** (-1.0)) - tau + 1
+    abs_dm1 = np.abs(delta - 1)
+    sgn = np.sign(delta - 1)
+    return (
+        -C4 * delta * n4 * (2 * delta - 2) * bd**b4 * phi
+        + b4
+        * delta
+        * n4
+        * bd ** (b4 - 1)
+        * (
+            2 * A4 * theta_val * abs_dm1 ** (-1 + beta4 ** (-1.0)) * sgn / beta4
+            + 2 * B4 * a4 * abs_dm1 ** (2 * a4 - 1) * sgn
+        )
+        * phi
+        + n4 * bd**b4 * phi
+    )
+
+
+def _s4_dd1_dt1(
+    tau: npt.NDArray[np.float64], delta: npt.NDArray[np.float64]
+) -> npt.NDArray[np.float64]:
+    bd = _s4_bigdelta(tau, delta)
+    phi = _s4_phi(delta, tau)
+    theta_val = A4 * np.abs(delta - 1) ** (beta4 ** (-1.0)) - tau + 1
+    abs_dm1 = np.abs(delta - 1)
+    sgn = np.sign(delta - 1)
+    d_bigdelta = A4 * theta_val * abs_dm1 ** (
+        beta4 ** (-1.0)
+    ) / beta4 + B4 * a4 * abs_dm1 ** (2 * a4)
+    return (
+        2
+        * n4
+        * bd**b4
+        * (
+            -A4 * b4 * delta * abs_dm1 ** (-1 + beta4 ** (-1.0)) * sgn / (beta4 * bd)
+            + 2 * C4 * D4 * delta * (delta - 1) * (tau - 1)
+            + 2 * C4 * b4 * delta * (delta - 1) * theta_val / bd
+            - 2 * D4 * b4 * delta * (tau - 1) * d_bigdelta * sgn / (bd * abs_dm1)
+            - D4 * (tau - 1)
+            - 2 * b4**2 * delta * d_bigdelta * theta_val * sgn / (bd**2 * abs_dm1)
+            + 2 * b4 * delta * d_bigdelta * theta_val * sgn / (bd**2 * abs_dm1)
+            - b4 * theta_val / bd
+        )
+        * phi
+    )
+
+
+def _s4_dd2_dt0(
+    tau: npt.NDArray[np.float64], delta: npt.NDArray[np.float64]
+) -> npt.NDArray[np.float64]:
+    bd = _s4_bigdelta(tau, delta)
+    phi = _s4_phi(delta, tau)
+    theta_val = A4 * np.abs(delta - 1) ** (beta4 ** (-1.0)) - tau + 1
+    abs_dm1 = np.abs(delta - 1)
+    sgn = np.sign(delta - 1)
+    dm1 = delta - 1
+    d_bigdelta = A4 * theta_val * abs_dm1 ** (
+        beta4 ** (-1.0)
+    ) / beta4 + B4 * a4 * abs_dm1 ** (2 * a4)
+    return (
+        2
+        * n4
+        * bd**b4
+        * (
+            -4 * C4 * b4 * delta * dm1 * d_bigdelta * sgn / (bd * abs_dm1)
+            + C4 * delta * (2 * C4 * dm1**2 - 1)
+            - 2 * C4 * dm1
+            + b4
+            * delta
+            * (
+                A4**2 * abs_dm1 ** (2 / beta4) * sgn**2 / (beta4**2 * dm1**2)
+                + 2
+                * A4
+                * theta_val
+                * abs_dm1 ** (-1 + beta4 ** (-1.0))
+                * _dirac_delta(dm1)
+                / beta4
+                - A4
+                * theta_val
+                * abs_dm1 ** (beta4 ** (-1.0))
+                * sgn**2
+                / (beta4 * dm1**2)
+                + A4
+                * theta_val
+                * abs_dm1 ** (beta4 ** (-1.0))
+                * sgn**2
+                / (beta4**2 * dm1**2)
+                + 2 * B4 * a4**2 * abs_dm1 ** (2 * a4) * sgn**2 / dm1**2
+                + 2 * B4 * a4 * abs_dm1 ** (2 * a4 - 1) * _dirac_delta(dm1)
+                - B4 * a4 * abs_dm1 ** (2 * a4) * sgn**2 / dm1**2
+                + 2 * b4 * d_bigdelta**2 * sgn**2 / (dm1**2 * bd)
+                - 2 * d_bigdelta**2 * sgn**2 / (dm1**2 * bd)
+            )
+            / bd
+            + 2 * b4 * d_bigdelta * sgn / (bd * abs_dm1)
+        )
+        * phi
+    )
+
+
+# ---------------------------------------------------------------------------
+# Dispatch table: maps (expression_index, diff_delta, diff_tau) to functions
+# expression_index: 1=s1, 2=s2, 3=s3, 4=s4
+# ---------------------------------------------------------------------------
+
+_EXPRESSIONS: dict[tuple[int, int, int], Callable[..., Any]] = {
+    (1, 0, 0): _s1_dd0_dt0,
+    (1, 0, 1): _s1_dd0_dt1,
+    (1, 0, 2): _s1_dd0_dt2,
+    (1, 1, 0): _s1_dd1_dt0,
+    (1, 1, 1): _s1_dd1_dt1,
+    (1, 2, 0): _s1_dd2_dt0,
+    (2, 0, 0): _s2_dd0_dt0,
+    (2, 0, 1): _s2_dd0_dt1,
+    (2, 0, 2): _s2_dd0_dt2,
+    (2, 1, 0): _s2_dd1_dt0,
+    (2, 1, 1): _s2_dd1_dt1,
+    (2, 2, 0): _s2_dd2_dt0,
+    (3, 0, 0): _s3_dd0_dt0,
+    (3, 0, 1): _s3_dd0_dt1,
+    (3, 0, 2): _s3_dd0_dt2,
+    (3, 1, 0): _s3_dd1_dt0,
+    (3, 1, 1): _s3_dd1_dt1,
+    (3, 2, 0): _s3_dd2_dt0,
+    (4, 0, 0): _s4_dd0_dt0,
+    (4, 0, 1): _s4_dd0_dt1,
+    (4, 0, 2): _s4_dd0_dt2,
+    (4, 1, 0): _s4_dd1_dt0,
+    (4, 1, 1): _s4_dd1_dt1,
+    (4, 2, 0): _s4_dd2_dt0,
+}
 
 
 def residual_helmholtz_energy(
@@ -29,87 +453,9 @@ def residual_helmholtz_energy(
 
     :return: Helmholtz free energy. Unit-less. numpy.ndarray with shape (N,)
     """
-    _s1 = np.sum(
-        _LAMBDIFIED_EXPRESSIONS[(s1, diff_delta, diff_tau)](tau_, delta_), axis=-1
-    )
-    _s2 = np.sum(
-        _LAMBDIFIED_EXPRESSIONS[(s2, diff_delta, diff_tau)](tau_, delta_), axis=-1
-    )
-    _s3 = np.sum(
-        _LAMBDIFIED_EXPRESSIONS[(s3, diff_delta, diff_tau)](tau_, delta_), axis=-1
-    )
-    _s4 = np.sum(
-        _LAMBDIFIED_EXPRESSIONS[(s4, diff_delta, diff_tau)](tau_, delta_), axis=-1
-    )
+    _s1 = np.sum(_EXPRESSIONS[(1, diff_delta, diff_tau)](tau_, delta_), axis=-1)
+    _s2 = np.sum(_EXPRESSIONS[(2, diff_delta, diff_tau)](tau_, delta_), axis=-1)
+    _s3 = np.sum(_EXPRESSIONS[(3, diff_delta, diff_tau)](tau_, delta_), axis=-1)
+    _s4 = np.sum(_EXPRESSIONS[(4, diff_delta, diff_tau)](tau_, delta_), axis=-1)
 
     return _s1 + _s2 + _s3 + _s4
-
-
-# Define coefficients symbols. These should correspond one-to-one with variable names in the coefficient module
-coeff_symbols = (
-    n1,
-    t1,
-    d1,
-    n2,
-    d2,
-    t2,
-    c2,
-    n3,
-    d3,
-    t3,
-    alpha3,
-    epsilon3,
-    beta3,
-    gamma3,
-    n4,
-    b4,
-    a4,
-    beta4,
-    A4,
-    B4,
-    C4,
-    D4,
-) = sp.symbols(
-    "n1 t1 d1 n2 d2 t2 c2 n3 d3 t3 alpha3 epsilon3 beta3 gamma3 n4 b4 a4 beta4 A4 B4 C4 D4"
-)
-tau, delta = sp.symbols("tau delta", real=True)
-i = sp.symbols("i", integer=True)
-s1 = n1 * delta**d1 * tau**t1
-s2 = n2 * delta**d2 * tau**t2 * sp.exp(-(delta**c2))
-s3 = (
-    n3
-    * delta**d3
-    * tau**t3
-    * sp.exp(-alpha3 * (delta - epsilon3) ** 2 - beta3 * (tau - gamma3) ** 2)
-)
-
-theta_expr = (1 - tau) + A4 * ((delta - 1) ** 2) ** (1 / (2 * beta4))
-bigdelta_expr = theta_expr**2 + B4 * ((delta - 1) ** 2) ** a4
-bigphi_expr = sp.exp(-C4 * (delta - 1) ** 2 - D4 * (tau - 1) ** 2)
-s4 = n4 * bigdelta_expr**b4 * delta * bigphi_expr
-
-
-def _lambdify(
-    expr: sp.Expr,
-    diff_delta: Literal[0, 1, 2],
-    diff_tau: Literal[0, 1, 2],
-) -> Callable[..., Any]:
-    diff = [delta] * diff_delta + [tau] * diff_tau
-    if len(diff) > 0:
-        expr = expr.diff(*diff)  # pyright: ignore[reportUnknownVariableType] | Sympy typing is incomplete
-    expr = expr.powsimp()  # pyright: ignore[reportUnknownVariableType, reportUnknownMemberType] | Sympy typing is incomplete
-    _sympy_lambda = sp.utilities.lambdify(  # pyright: ignore[reportUnknownVariableType] | Sympy typing is incomplete
-        list(coeff_symbols) + [tau, delta],
-        expr,
-        modules=["numpy", {"DiracDelta": lambda x: x == 0}],  # pyright: ignore[reportUnknownLambdaType] | Sympy typing is incomplete
-    )
-    return lambda _tau, _delta: _sympy_lambda(*coeff_vars, _tau, _delta)  # pyright: ignore[reportUnknownVariableType, reportUnknownLambdaType] | Sympy typing is incomplete
-
-
-_LAMBDIFIED_EXPRESSIONS: dict[tuple[Any, int, int], Callable[..., Any]] = {
-    (e, dd, dt): _lambdify(expr=e, diff_delta=dd, diff_tau=dt)
-    for e in (s1, s2, s3, s4)
-    for dd in (0, 1, 2)
-    for dt in (0, 1, 2)
-    if dd + dt <= 2
-}
