5fec09c2fd3dc7cb77e58789a18ae41e38617266
Harald Flesche <hfle@equinor.com>
2026-02-18 10:57:19 +0100

    test: add tests for Han & Batzle models, update test snapshots

--- tests/fluid_model_tests/test_oil_properties.py
+++ tests/fluid_model_tests/test_oil_properties.py
@@ -10,8 +10,13 @@ from rock_physics_open.equinor_utilities.snapshot_test_utilities import (
     store_snapshot,
 )
 from rock_physics_open.fluid_models import oil_properties
+from rock_physics_open.fluid_models.oil_model.han_batzle_oil_model import (
+    han_batzle_live_oil_density,
+    han_batzle_live_oil_velocity,
+)
 from rock_physics_open.fluid_models.oil_model.live_oil_density import live_oil_density
 from rock_physics_open.fluid_models.oil_model.live_oil_velocity import live_oil_velocity
+from rock_physics_open.fluid_models.oil_model.oil_properties import live_oil
 
 temp = 100.0 * np.linspace(0.8, 1.2, 101)
 pres = 30.0e6 * np.linspace(0.8, 1.2, 101)
@@ -24,6 +29,15 @@ gr_co2 = 1.52 * np.ones(101)
 
 def test_oil_prop():
     args = oil_properties(temp, pres, rho_0, gor, gr)
+
+    if not os.path.isfile(get_snapshot_name()) or INITIATE:
+        _ = store_snapshot(get_snapshot_name(), *args)
+    else:
+        assert compare_snapshots(args, read_snapshot(get_snapshot_name()))
+
+
+def test_oil_prop_bw():
+    args = oil_properties(temp, pres, rho_0, gor, gr, model_version="BW")
 
     if not os.path.isfile(get_snapshot_name()) or INITIATE:
         _ = store_snapshot(get_snapshot_name(), *args)
@@ -49,14 +63,67 @@ def test_live_oil_velocity():
         assert compare_snapshots(args, read_snapshot(get_snapshot_name()))
 
 
+def test_han_batzle_live_oil_density():
+    args = han_batzle_live_oil_density(temp, pres, rho_0, gor, gr)
+
+    if not os.path.isfile(get_snapshot_name()) or INITIATE:
+        _ = store_snapshot(get_snapshot_name(), args)
+    else:
+        assert compare_snapshots(args, read_snapshot(get_snapshot_name()))
+
+
+def test_han_batzle_live_oil_velocity():
+    args = han_batzle_live_oil_velocity(rho_0, gor, gr, temp, pres)
+
+    if not os.path.isfile(get_snapshot_name()) or INITIATE:
+        _ = store_snapshot(get_snapshot_name(), args)
+    else:
+        assert compare_snapshots(args, read_snapshot(get_snapshot_name()))
+
+
+def test_live_oil_han_batzle():
+    args = live_oil(
+        temperature=temp,
+        pressure=pres,
+        reference_density=rho_0,
+        gas_oil_ratio=gor,
+        gas_gravity=gr,
+        model_version="HB",
+    )
+
+    if not os.path.isfile(get_snapshot_name()) or INITIATE:
+        _ = store_snapshot(get_snapshot_name(), *args)
+    else:
+        assert compare_snapshots(args, read_snapshot(get_snapshot_name()))
+
+
 def test_oil_properties_float():
     """
     Make sure that input object type is reflected in output type
     """
     for results in [
         oil_properties(temp[0], pres[0], rho_0[0], gor[0], gr[0]),
+        oil_properties(temp[0], pres[0], rho_0[0], gor[0], gr[0], model_version="BW"),
         live_oil_density(temp[0], pres[0], rho_0[0], gor[0], gr[0]),
         live_oil_velocity(temp[0], pres[0], rho_0[0], gor[0], gr[0]),
+        han_batzle_live_oil_density(temp[0], pres[0], rho_0[0], gor[0], gr[0]),
+        han_batzle_live_oil_velocity(rho_0[0], gor[0], gr[0], temp[0], pres[0]),
+        live_oil(
+            temperature=temp[0],
+            pressure=pres[0],
+            reference_density=rho_0[0],
+            gas_oil_ratio=gor[0],
+            gas_gravity=gr[0],
+            model_version="HB",
+        ),
+        live_oil(
+            temperature=temp[0],
+            pressure=pres[0],
+            reference_density=rho_0[0],
+            gas_oil_ratio=gor[0],
+            gas_gravity=gr[0],
+            model_version="BW",
+        ),
     ]:
         if hasattr(results, "__iter__"):
             assert all(isinstance(arg, float) for arg in results)
