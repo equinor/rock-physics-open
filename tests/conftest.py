7e716ef33e59d1dd65e27565568b74f0d331e128
sutne <sutn@equinor.com>
2026-04-27 07:10:15 +0200

    test: remove __init__.py files from tests
    To align with more modern pytest practices

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,5 +1,5 @@
 # load and use fixtures from the `fixtures` directory
 pytest_plugins = [
-    "tests.fixtures.snapshot",
-    "tests.fixtures.testdata",
+    "fixtures.snapshot",
+    "fixtures.testdata",
 ]
