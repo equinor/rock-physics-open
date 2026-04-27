6ef79d036680285e727e26bb7f0d6bb45b112140
sutne <sutn@equinor.com>
2026-04-27 07:29:22 +0200

    test: replace joindir with '/'
    More robust and modern operator that handles strings as well as other paths and objects.

--- tests/t_matrix_model_tests/test_opt_param_to_ascii.py
+++ tests/t_matrix_model_tests/test_opt_param_to_ascii.py
@@ -6,12 +6,12 @@ from rock_physics_open.equinor_utilities.optimisation_utilities import (
 
 
 def test_opt_param_to_ascii_display_petec(testdata: Path):
-    in_file = testdata.joinpath("petec_opt_param_test.pkl")
+    in_file = testdata / "petec_opt_param_test.pkl"
     try:
         opt_param_to_ascii(in_file, display_results=False)
     except (ValueError, IOError):
         raise ValueError(f"Not possible to read input file {in_file}")
-    out_file = testdata.joinpath("petec_opt_param.txt")
+    out_file = testdata / "petec_opt_param.txt"
     try:
         opt_param_to_ascii(in_file, display_results=False, out_file=out_file)
     except (ValueError, IOError):
@@ -19,12 +19,12 @@ def test_opt_param_to_ascii_display_petec(testdata: Path):
 
 
 def test_opt_param_to_ascii_display_exp(testdata: Path):
-    in_file = testdata.joinpath("exp_opt_param_test.pkl")
+    in_file = testdata / "exp_opt_param_test.pkl"
     try:
         opt_param_to_ascii(in_file, display_results=False)
     except (ValueError, IOError):
         raise ValueError(f"Not possible to read input file {in_file}")
-    out_file = testdata.joinpath("exp_opt_param.txt")
+    out_file = testdata / "exp_opt_param.txt"
     try:
         opt_param_to_ascii(in_file, display_results=False, out_file=out_file)
     except (ValueError, IOError):
