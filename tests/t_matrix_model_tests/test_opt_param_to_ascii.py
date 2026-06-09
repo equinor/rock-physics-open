fe54336b64ca656cd9af6b5efc0756c7a3be76b5
sutne <sutn@equinor.com>
2026-06-09 15:51:43 +0200

    test: simplify opt_param_to_ascii test

--- tests/t_matrix_model_tests/test_opt_param_to_ascii.py
+++ tests/t_matrix_model_tests/test_opt_param_to_ascii.py
@@ -5,27 +5,19 @@ from rock_physics_open.equinor_utilities.optimisation_utilities import (
 )
 
 
-def test_opt_param_to_ascii_display_petec(testdata: Path):
-    in_file = testdata / "petec_opt_param_test.pkl"
-    try:
-        opt_param_to_ascii(in_file, display_results=False)
-    except (ValueError, IOError):
-        raise ValueError(f"Not possible to read input file {in_file}")
-    out_file = testdata / "petec_opt_param.txt"
-    try:
-        opt_param_to_ascii(in_file, display_results=False, out_file=out_file)
-    except (ValueError, IOError):
-        raise ValueError(f"Not possible to write output file {out_file}")
+def test_opt_param_to_ascii_petec(testdata: Path):
+    tmatrix_parameters_petec = testdata / "petec_opt_param_test.pkl"
+    opt_param_to_ascii(
+        tmatrix_parameters_petec,
+        display_results=False,
+        out_file=testdata / "petec_opt_param.txt",
+    )
 
 
-def test_opt_param_to_ascii_display_exp(testdata: Path):
-    in_file = testdata / "exp_opt_param_test.pkl"
-    try:
-        opt_param_to_ascii(in_file, display_results=False)
-    except (ValueError, IOError):
-        raise ValueError(f"Not possible to read input file {in_file}")
-    out_file = testdata / "exp_opt_param.txt"
-    try:
-        opt_param_to_ascii(in_file, display_results=False, out_file=out_file)
-    except (ValueError, IOError):
-        raise ValueError(f"Not possible to write output file {out_file}")
+def test_opt_param_to_ascii_exp(testdata: Path):
+    tmatrix_parameters_exp = testdata / "exp_opt_param_test.pkl"
+    opt_param_to_ascii(
+        tmatrix_parameters_exp,
+        display_results=False,
+        out_file=testdata / "exp_opt_param.txt",
+    )
