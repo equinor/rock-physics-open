1a7c206599423f91b092ad071c6ff6fe6ec59ae0
sutne <sutn@equinor.com>
2026-06-10 09:45:42 +0200

    chore: update test value to not be impacted by invariance in numpy versions

--- tests/classification_functions_tests/test_two_step_class.py
+++ tests/classification_functions_tests/test_two_step_class.py
@@ -10,5 +10,5 @@ from rock_physics_open.equinor_utilities.classification_functions import (
 def test_mahal_class_thresh(snapshot: SnapshotAssertion):
     rg = default_rng(238476)
     obs = rg.random((11, 2))
-    inp_class_id = np.array([3, 3, 1, 2, 3, 1, 3, 3, 2, 3, 1])
+    inp_class_id = np.array([3, 3, 1, 2, 3, 1, 3, 2, 2, 3, 1])
     assert snapshot == gen_two_step_class_stats(obs, inp_class_id, thresh=1.5)
