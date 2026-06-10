de2986c8056346f40668df041430e84260cb8b7f
sutne <sutn@equinor.com>
2026-06-10 09:04:57 +0200

    test: use snapshot for opt_param_to_ascii tests

--- tests/t_matrix_model_tests/test_opt_param_to_ascii.py
+++ tests/t_matrix_model_tests/test_opt_param_to_ascii.py
@@ -1,23 +1,37 @@
 from pathlib import Path
 
+from syrupy.assertion import SnapshotAssertion
+
 from rock_physics_open.equinor_utilities.optimisation_utilities import (
     opt_param_to_ascii,
 )
 
 
-def test_opt_param_to_ascii_petec(testdata: Path, tmp_path: Path):
+def test_opt_param_to_ascii_petec(
+    snapshot: SnapshotAssertion,
+    testdata: Path,
+    tmp_path: Path,
+):
     tmatrix_parameters_petec = testdata / "petec_opt_param_test.pkl"
+    output_ascii_file = tmp_path / "petec_opt_param.txt"
     opt_param_to_ascii(
         tmatrix_parameters_petec,
         display_results=False,
-        out_file=tmp_path / "petec_opt_param.txt",
+        out_file=output_ascii_file,
     )
+    assert snapshot == output_ascii_file.read_text().strip()
 
 
-def test_opt_param_to_ascii_exp(testdata: Path, tmp_path: Path):
-    tmatrix_parameters_exp = testdata / "exp_opt_param_test.pkl"
+def test_opt_param_to_ascii_exp(
+    snapshot: SnapshotAssertion,
+    testdata: Path,
+    tmp_path: Path,
+):
+    tmatrix_parameters_exp: Path = testdata / "exp_opt_param_test.pkl"
+    output_ascii_file = tmp_path / "exp_opt_param.txt"
     opt_param_to_ascii(
         tmatrix_parameters_exp,
         display_results=False,
-        out_file=tmp_path / "exp_opt_param.txt",
+        out_file=output_ascii_file,
     )
+    assert snapshot == output_ascii_file.read_text().strip()
